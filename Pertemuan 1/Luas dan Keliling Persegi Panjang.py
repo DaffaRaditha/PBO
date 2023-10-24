39e2c94f2e44124e55883847ea58cc9d39a6c5e4
DaffaRaditha <148331475+DaffaRaditha@users.noreply.github.com>
2023-10-24 09:57:57 +0700

    Add files via upload

---
+++ Pertemuan 1/Luas dan Keliling Persegi Panjang.py
@@ -0,0 +1,15 @@
+print("Menghitung Luas dan Keliling Persegi Panjang")
+
+# Nilai
+panjang = 10
+lebar = 5
+
+# Rumus
+luas = panjang * lebar
+keliling = (2 * panjang) + (2 * lebar)
+
+# Output
+print("Panjang = ", panjang)
+print("Lebar = ", lebar)
+print("Luas = ", luas)
+print("Keliling = ", keliling)
