96fe28204691d9c018de49f9c7469abeb074c813
DaffaRaditha <148331475+DaffaRaditha@users.noreply.github.com>
2023-10-24 10:04:16 +0700

    Add files via upload

---
+++ Pertemuan 1/Luas dan Volume Balok.py
@@ -0,0 +1,17 @@
+print("Menghitung Luas Dan Volume Balok")
+
+# Nilai
+panjang = 10
+lebar = 7
+tinggi = 5
+
+# Rumus
+luas = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi))
+volume = panjang * lebar * tinggi
+
+# Output
+print("Panjang = ", panjang)
+print("Lebar = ", lebar)
+print("Tinggi = ", tinggi)
+print("Luas = ", luas)
+print("Volume = ", volume)
---
+++ Pertemuan 1/Luas dan Volume Bola.py
@@ -0,0 +1,13 @@
+print("Menghitung Luas Dan Volume Bola")
+
+# Nilai
+jari_jari = 5
+
+# Rumus
+luas = 4 * 3.14 * jari_jari**2
+volume = (4/3) * 3.14 * jari_jari**3
+
+# Output
+print("Jari-jari = ", jari_jari)
+print("Luas = ", luas)
+print("Volume = ", volume)
