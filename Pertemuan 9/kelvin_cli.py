b6dfb876b8d81eadcad3a88e58d5a4559ed61009
DaffaRaditha <148331475+DaffaRaditha@users.noreply.github.com>
2023-12-25 23:18:40 +0700

    Add files via upload

---
+++ Pertemuan 9/kelvin_cli.py
@@ -0,0 +1,16 @@
+print("Konversi Suhu Kelvin")
+
+# Entry
+suhu = input("Masukkan suhu dalam Kelvin:")
+
+
+# rumus
+F = (9/5 * (float(suhu) - 273)) + 32
+R = (4/5 * (float(suhu) - 273))
+C = float(suhu) - 273
+
+#output
+print(suhu + " kelvin sama dengan ")
+print(str(F) + " Fahrenheit")
+print(str(R) + " Reamur")
+print(str(C) + " Celcius")
